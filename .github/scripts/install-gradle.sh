ab4b5d09aa24e2398d046d02c0fae259618b16da
John Gallagher <john.gallagher@delphix.com>
2020-01-15 14:36:19 -0800

    Upgrade Gradle to latest version

--- .github/scripts/install-gradle.sh
+++ .github/scripts/install-gradle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-wget -nv https://services.gradle.org/distributions/gradle-5.1-bin.zip
-sha256sum -c <<< '7506638a380092a0406364c79d6c87d03d23017fc25a5770379d1ce23c3fcd4d  gradle-5.1-bin.zip'
-unzip -d /opt gradle-5.1-bin.zip
-rm gradle-5.1-bin.zip
+wget -nv https://services.gradle.org/distributions/gradle-6.0.1-bin.zip
+sha256sum -c <<< 'd364b7098b9f2e58579a3603dc0a12a1991353ac58ed339316e6762b21efba44  gradle-6.0.1-bin.zip'
+unzip -d /opt gradle-6.0.1-bin.zip
+rm gradle-6.0.1-bin.zip
